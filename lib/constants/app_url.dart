2817c71733e7131fa9ae099022f7deea55b9958e
jsefao <55448613+jse011@users.noreply.github.com>
2025-05-24 18:37:02 -0500

    mejoras de navegacion

--- lib/constants/app_url.dart
+++ lib/constants/app_url.dart
@@ -1,3 +1,3 @@
 class AppUrl{
-  static const pdfPath = 'https://jovenesapce.github.io/living-worship-app/pdf/GUIA_DE_ORIENTACIONES_LIVING_WORSHIP.pdf';
+  static const pdfPath = 'https://jovenesapce.github.io/pdf/GUIA_DE_ORIENTACIONES_LIVING_WORSHIP.pdf';
 }
