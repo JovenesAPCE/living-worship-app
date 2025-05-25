61899a943e31d906ae4ddcb1fc3f067f9f6cf2b1
jse011 <jse011@users.noreply.github.com>
2025-05-25 23:38:48 +0000

    deploy: 26769b1d3b96a3fd2ada926ad817de2026ad75ec

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1646970918"
+    serviceWorkerVersion: "3683454204"
   }
 });
