c00ce75c78c56b9b33f75fef4341f858e0f91350
jsefao <55448613+jse011@users.noreply.github.com>
2025-05-25 15:28:58 -0500

    mejoras de navegacion

--- lib/feature/login/view/login_page.dart
+++ lib/feature/login/view/login_page.dart
@@ -18,6 +18,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     backgroundColor: Colors.black,
       resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
