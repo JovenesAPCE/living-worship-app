518dc8243c1b55c7c5f6320464851bb482e3f8e5
jsefao <55448613+jse011@users.noreply.github.com>
2025-05-30 17:17:35 -0500

    mejoras de navegacion

--- lib/constants/app_url.dart
+++ lib/constants/app_url.dart
@@ -1,5 +1,5 @@
 class AppUrl{
-  static const pdfPath = 'https://jovenesapce.github.io/living-worship-app/pdf/GUÍA_DE_ORIENTACIONES_LIVING_WORSHIP_NEW.pdf';
+  static const pdfPath = 'https://jovenesapce.github.io/living-worship-app/pdf/GUIA_DE_ORIENTACIONES_LIVING_WORSHIP_NEW.pdf';
   static const manaPath = 'https://www.sehsnorte.net/mana/';
   static const updateUserAsd7 = 'https://adv.pe/formulario';
 }
